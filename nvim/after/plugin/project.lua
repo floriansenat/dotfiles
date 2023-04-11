b170cbb98f15d632f652d781d6af7fd4a4b5c9f3
floriansenat <florian.senat@proton.me>
2023-04-11 14:18:35 +0200

    Remove nvim-tree

--- nvim/after/plugin/project.lua
+++ nvim/after/plugin/project.lua
@@ -1,7 +1,4 @@
 local wk = require('which-key')
-local nvimTree = require('nvim-tree.api')
 
 wk.register({ ['<leader>p'] = { name = '[P]roject' } })
 vim.keymap.set('n', '<leader>pe', vim.cmd.Ex, { silent = true, desc = "Show [E]xplorer" })
-vim.keymap.set('n', '<leader>pv', nvimTree.tree.toggle, { silent = true, desc = "Show [V]iew" })
-vim.keymap.set('n', '<leader>pr', nvimTree.tree.reload, { silent = true, desc = "[R]eload" })
