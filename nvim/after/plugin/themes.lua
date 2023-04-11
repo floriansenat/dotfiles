867631ed72fae84a57d768caa18baa7cb980b55b
floriansenat <florian.senat@proton.me>
2023-04-11 17:33:04 +0200

    Update theme plugin

--- nvim/after/plugin/themes.lua
+++
@@ -1,6 +0,0 @@
-local wk = require('which-key')
-
-wk.register({ ['<leader>T'] = { name = '[T]heme' } })
-vim.keymap.set('n', '<leader>Td', '<cmd>set background=dark<CR>', { desc = "[D]ark" })
-vim.keymap.set('n', '<leader>Tl', '<cmd>set background=light<CR>', { desc = "[L]ight" })
-
--- nvim/lua/custom/plugins/themes.lua
+++ nvim/lua/custom/plugins/themes.lua
@@ -1,9 +1,24 @@
+local is_dark = true
+
+local function get_theme()
+	if is_dark then return 'dark' end
+	return 'light'
+end
+
+local function toggle_theme()
+	is_dark = not is_dark
+	vim.o.background = get_theme()
+end
+
 return {
+
 	{
 		'sainnhe/everforest',
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme 'everforest'
+			vim.cmd.colorscheme('everforest')
+			vim.api.nvim_create_user_command('ToggleTheme', toggle_theme, {})
+			vim.keymap.set('n', '<leader>t', '<cmd>ToggleTheme<CR>', { desc = 'Toggle [T]heme' })
 		end,
 	}
 }
