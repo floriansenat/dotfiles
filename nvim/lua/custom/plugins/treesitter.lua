22031d6064fba72940984b7c2de31785e823f735
floriansenat <florian.senat@proton.me>
2023-04-11 14:30:12 +0200

    Remove useless after treesitter plugin

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,11 +1,50 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
+		dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
 		config = function()
-			pcall(require('nvim-treesitter.install').update { with_sync = true })
+			pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+
+			require('nvim-treesitter.configs').setup({
+				ensure_installed = {
+					'lua',
+					'rust',
+					'tsx',
+					'typescript',
+					'help',
+					'vim',
+					'markdown',
+					'markdown_inline',
+					'html',
+					'javascript',
+					'json',
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						-- init_selection = '<c-space>',
+						-- node_incremental = '<c-space>',
+						scope_incremental = '<c-s>',
+						-- node_decremental = '<M-space>',
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							['aa'] = '@parameter.outer',
+							['ia'] = '@parameter.inner',
+							['af'] = '@function.outer',
+							['if'] = '@function.inner',
+							['ac'] = '@class.outer',
+							['ic'] = '@class.inner',
+						},
+					},
+				},
+			})
 		end,
 	}
 }
