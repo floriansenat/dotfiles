647f9a1fa35862a8fed40842c4dd51780b93b266
floriansenat <florian.senat@proton.me>
2023-09-19 13:52:56 +0200

    Add cmp icons

--- nvim/lua/custom/plugins/cmp.lua
+++ nvim/lua/custom/plugins/cmp.lua
@@ -1,3 +1,31 @@
+local cmp_kinds = {
+	Text = '  ',
+	Method = '  ',
+	Function = '  ',
+	Constructor = '  ',
+	Field = '  ',
+	Variable = '  ',
+	Class = '  ',
+	Interface = '  ',
+	Module = '  ',
+	Property = '  ',
+	Unit = '  ',
+	Value = '  ',
+	Enum = '  ',
+	Keyword = '  ',
+	Snippet = '  ',
+	Color = '  ',
+	File = '  ',
+	Reference = '  ',
+	Folder = '  ',
+	EnumMember = '  ',
+	Constant = '  ',
+	Struct = '  ',
+	Event = '  ',
+	Operator = '  ',
+	TypeParameter = '  ',
+}
+
 return {
 	'hrsh7th/nvim-cmp',
 	dependencies = {
@@ -45,6 +73,12 @@ return {
 			sources = {
 				{ name = 'nvim_lsp' },
 				{ name = 'luasnip' },
+			},
+			formatting = {
+				format = function(_, vim_item)
+					vim_item.kind = (cmp_kinds[vim_item.kind] or '') .. vim_item.kind
+					return vim_item
+				end,
 			},
 		})
 	end
