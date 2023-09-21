58e61206ab137030e51855387b52c60caaa93669
floriansenat <florian.senat@proton.me>
2023-09-21 10:42:58 +0200

    Remove transparent group

--- nvim/lua/custom/plugins/transparent.lua
+++ nvim/lua/custom/plugins/transparent.lua
@@ -1,8 +1,1 @@
-return {
-	"xiyaowong/transparent.nvim",
-	opts = {
-		extra_groups = {
-			"NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-		},
-	}
-}
+return { "xiyaowong/transparent.nvim" }
