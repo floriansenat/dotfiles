eae99609b8e333e98460a0c4f7555c63151b4077
floriansenat <florian.senat@proton.me>
2023-09-26 19:40:36 +0200

    Add deno lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,28 +1,39 @@
-local servers = {
-	tsserver = {},
-	lua_ls = {
-		Lua = {
-			yorkspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-		},
-	}
-}
+require 'neodev'.setup()
 
-require('neodev').setup()
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require 'cmp_nvim_lsp'.default_capabilities(capabilities)
 
-require('mason').setup()
 
-local mason_lspconfig = require('mason-lspconfig')
-mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
-
-mason_lspconfig.setup_handlers({
+local lspconfig = require 'lspconfig'
+local handlers = {
 	function(server_name)
-		require('lspconfig')[server_name].setup({
-			capabilities = capabilities,
-			settings = servers[server_name]
-		})
+		lspconfig[server_name].setup {
+			capabilities = capabilities
+		}
+	end,
+	['denols'] = function()
+		lspconfig.denols.setup {
+			root_dir = lspconfig.util.root_pattern "deno.json", "deno.jsonc",
+		}
+	end,
+	['tsserver'] = function()
+		lspconfig.tsserver.setup {
+			root_dir = lspconfig.util.root_pattern "package.json", "tsconfig.json", "jsconfig.json",
+			single_file_support = false
+		}
+	end,
+	['lua_ls'] = function()
+		lspconfig.lua_ls.setup {
+			settings = {
+				Lua = {
+					yorkspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+				},
+			}
+		}
 	end
-})
+}
+
+require 'mason'.setup()
+require 'mason-lspconfig'.setup { ensure_installed = { "lua_ls", "tsserver", "denols" }, handlers = handlers }
