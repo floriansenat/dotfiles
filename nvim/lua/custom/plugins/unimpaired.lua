11a899aeabc4b6cd3697677392e784073e17dd2c
floriansenat <florian.senat@proton.me>
2024-03-01 09:14:49 +0100

    Remove unused nvim plugins

--- nvim/lua/custom/plugins/unimpaired.lua
+++
@@ -1,7 +0,0 @@
-return {
-	'tpope/vim-unimpaired',
-	config = function()
-		vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle, { desc = '[T]oggle' })
-		vim.keymap.set('n', '<leader>uf', vim.cmd.UndotreeFocus, { desc = '[F]ocus' })
-	end
-}
