0d9bd6cd101141cbdd3aa59add91d8ebfe0f8d33
floriansenat <github.conductor645@passmail.net>
2024-05-30 09:24:35 +0200

    Update nvim git keymap

--- nvim/lua/custom/plugins/git.lua
+++ nvim/lua/custom/plugins/git.lua
@@ -5,14 +5,14 @@ return {
 			on_attach = function()
 				local gs = package.loaded.gitsigns
 
-				vim.keymap.set('n', ']h', function()
-					if vim.wo.diff then return ']h' end
+				vim.keymap.set('n', ']c', function()
+					if vim.wo.diff then return ']c' end
 					vim.schedule(function() gs.next_hunk() end)
 					return '<Ignore>'
 				end, { expr = true, desc = 'Next [H]unk' })
 
-				vim.keymap.set('n', '[h', function()
-					if vim.wo.diff then return '[h' end
+				vim.keymap.set('n', '[c', function()
+					if vim.wo.diff then return '[c' end
 					vim.schedule(function() gs.prev_hunk() end)
 					return '<Ignore>'
 				end, { expr = true, desc = 'Previous [H]unk' })
