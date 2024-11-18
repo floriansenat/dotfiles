1d181c085894e90f6d7e60a946e1b56a200c12bd
floriansenat <github.conductor645@passmail.net>
2024-11-18 17:47:28 +0100

    Add wezterm workspaces management

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local color_scheme = require 'color_scheme'
+local workspaces_keys = require 'workspaces'
 
+local action = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
@@ -23,39 +25,15 @@ config.font_size = 15.0
 config.line_height = 1.4
 
 config.keys = {
-  -- Vertical split w/ CTRL+SHIFT+"
-  {
-    key = '"',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  -- Horizontal split w/ CTRL+SHIFT+:
-  {
-    key = ':',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  -- Split navigation
-  {
-    key = 'h',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'l',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'k',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'CTRL|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
+  -- Panes management
+  { key = '"', mods = 'CTRL|SHIFT', action = action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = ':', mods = 'CTRL|SHIFT', action = action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'h', mods = 'CTRL|SHIFT', action = action.ActivatePaneDirection 'Left' },
+  { key = 'l', mods = 'CTRL|SHIFT', action = action.ActivatePaneDirection 'Right' },
+  { key = 'k', mods = 'CTRL|SHIFT', action = action.ActivatePaneDirection 'Up' },
+  { key = 'j', mods = 'CTRL|SHIFT', action = action.ActivatePaneDirection 'Down' },
+  -- Workspaces management
+  table.unpack(workspaces_keys),
 }
 
 return config
