d17e1fa376997acca4245aa5970ad39014c61613
floriansenat <github.conductor645@passmail.net>
2024-12-10 11:18:47 +0100

    add biome lsp

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -5,12 +5,6 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { 'prettier' },
-      javascriptreact = { 'prettier' },
-      typescript = { 'prettier' },
-      typescriptreact = { 'prettier' },
-      json = { 'prettier' },
-      go = { 'gofmt' },
     },
     default_format_opts = {
       lsp_format = 'fallback',
