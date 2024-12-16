83ec3ec0b0f9ffe211fd89ec722c487b4c55edc6
floriansenat <github.conductor645@passmail.net>
2024-12-16 11:54:08 +0100

    fix always update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,21 +3,19 @@ return {
   build = ':TSUpdate',
   dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
   config = function()
-    pcall(require('nvim-treesitter.install').update { with_sync = true })
-
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
         'lua',
-        'rust',
         'tsx',
         'typescript',
+        'javascript',
+        'json',
+        'html',
+        'css',
         'help',
         'vim',
         'markdown',
         'markdown_inline',
-        'html',
-        'javascript',
-        'json',
       },
       highlight = { enable = true },
       indent = { enable = true },
