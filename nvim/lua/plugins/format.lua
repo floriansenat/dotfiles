1dd8cab0ed4f7adee9572b9d15000870c3775e29
floriansenat <github.conductor645@passmail.net>
2024-12-17 17:55:00 +0100

    update format to avoid biome/ prettier conflict

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -5,10 +5,6 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { 'biome', 'prettier', stop_after_first = true },
-      javascriptreact = { 'biome', 'prettier', stop_after_first = true },
-      typescript = { 'biome', 'prettier', stop_after_first = true },
-      typescriptreact = { 'biome', 'prettier', stop_after_first = true },
     },
     default_format_opts = {
       lsp_format = 'fallback',
