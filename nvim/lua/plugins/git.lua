fcba5e7edd51b8d4ab71832a35edb10408cbc6a8
floriansenat <github.conductor645@passmail.net>
2025-01-07 19:54:50 +0100

    update neovim plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,8 +3,8 @@ return {
   event = 'VeryLazy',
   opts = {},
   keys = {
-    { ']c', ':Gitsigns next_hunk<CR>', desc = 'Next [C]hange' },
-    { '[c', ':Gitsigns prev_hunk<CR>', desc = 'Previous [C]hange' },
+    { ']c', ':Gitsigns next_hunk<CR><CR>', desc = 'Next [C]hange' },
+    { '[c', ':Gitsigns prev_hunk<CR><CR>', desc = 'Previous [C]hange' },
     { '<leader>gh', ':Gitsigns preview_hunk_inline<CR>', desc = '[H]unk' },
     { '<leader>gb', ':Gitsigns blame<CR>', desc = '[B]lame' },
     { '<leader>gr', ':Gitsigns reset_hunk<CR>', desc = '[R]eset hunk' },
