fa56b512c60ff67713f87befa6afc32f5bf5e2ae
floriansenat <github.conductor645@passmail.net>
2025-02-18 09:58:29 +0100

    add codestral for ai completion

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,5 +1,45 @@
-local adapter = 'anthropic'
-local model = 'claude-3-5-sonnet-20241022'
+local adapter = 'openai'
+
+local function codestral_adapter()
+  return require('codecompanion.adapters').extend('openai_compatible', {
+    name = 'codestral',
+    env = {
+      api_key = 'CODESTRAL_API_KEY',
+      url = 'https://codestral.mistral.ai',
+      char_url = '/v1/chat/completions',
+    },
+    handlers = {
+      form_parameters = function(_, params, _)
+        -- codestral doesn't support these in the body
+        params.stream_options = nil
+        params.options = nil
+
+        return params
+      end,
+    },
+    schema = {
+      model = { default = 'codestral-latest' },
+      temperature = {
+        default = 0.2,
+        mapping = 'parameters', -- not supported in default parameters.options
+      },
+    },
+  })
+end
+
+local function anthropic_adapter()
+  return require('codecompanion.adapters').extend(adapter, {
+    env = { api_key = 'ANTHROPIC_API_KEY' },
+    schema = {
+      model = { default = 'claude-3-5-sonnet-20241022' },
+    },
+  })
+end
+
+local ADAPTERS = {
+  codestral = codestral_adapter,
+  anthropic = anthropic_adapter,
+}
 
 return {
   'olimorris/codecompanion.nvim',
@@ -13,16 +53,8 @@ return {
         chat = { adapter = adapter },
         inline = { adapter = adapter },
       },
-      adapters = {
-        anthropic = function()
-          return require('codecompanion.adapters').extend(adapter, {
-            env = { api_key = 'ANTHROPIC_API_KEY' },
-            schema = {
-              model = { default = model },
-            },
-          })
-        end,
-      },
+      opts = { stream = true },
+      adapters = ADAPTERS[adapter],
     }
   end,
   keys = {
