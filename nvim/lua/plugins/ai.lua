d268275fa9a0c613dd36f12ecda608d173644d62
Florian SENAT <floriansenat@Florians-MacBook-Pro.local>
2025-03-11 21:48:17 +0100

    add avante for ai

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,62 +1,61 @@
-local function lm_studio_adapter()
-  return require('codecompanion.adapters').extend('openai_compatible', {
-    name = 'lm_studio',
-    env = {
-      url = 'http://localhost:1234',
-    },
-    schema = {
-      model = { default = 'deepseek-r1-distill-qwen-7b' },
-    },
-  })
-end
-
-local function codestral_adapter()
-  return require('codecompanion.adapters').extend('openai_compatible', {
-    name = 'codestral',
-    env = {
-      api_key = 'CODESTRAL_API_KEY',
-      url = 'https://codestral.mistral.ai',
-    },
-    handlers = {
-      form_parameters = function(_, params, _)
-        -- codestral doesn't support these in the body
-        params.stream_options = nil
-        params.options = nil
-
-        return params
-      end,
-    },
-    schema = {
-      model = { default = 'codestral-latest' },
-      temperature = {
-        default = 0.2,
-        mapping = 'parameters', -- not supported in default parameters.options
-      },
-    },
-  })
-end
-
 return {
-  'olimorris/codecompanion.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  version = '*',
+  opts = {
+    provider = 'claude',
+    claude = {
+      endpoint = 'https://api.anthropic.com',
+      model = 'claude-3-7-sonnet-20250219',
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    openai = {
+      endpoint = 'https://api.openai.com/v1',
+      model = 'gpt-4o',
+      timeout = 30000,
+      temperature = 0,
+      max_tokens = 4096,
+      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+    },
+    windows = {
+      width = 40,
+      sidebar_header = {
+        enabled = true,
+        align = 'left', -- (left, center, right)
+        rounded = false,
+      },
+    },
   },
-  config = function()
-    require('codecompanion').setup {
-      strategies = {
-        chat = { adapter = 'openai' },
-        inline = { adapter = 'openai' },
+  build = 'make',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    {
+      -- Support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
       },
-      opts = { stream = true },
-      adapters = {
-        anthropic = 'anthropic',
-        openai = lm_studio_adapter(),
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
       },
-    }
-  end,
-  keys = {
-    { '<leader>a', '<cmd>CodeCompanionChat Toggle<cr>', mode = { 'n', 'v' } },
-    { 'ga', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' } },
+      ft = { 'markdown', 'Avante' },
+    },
   },
 }
