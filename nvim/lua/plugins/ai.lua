d6b89263c04e37318f6e3765d1cf7af3e3d27e5b
Florian SENAT <floriansenat@Florians-MacBook-Pro.local>
2025-03-19 09:12:00 +0100

    update ai spinner and and llm.nvim

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,47 +1,59 @@
 return {
-  'olimorris/codecompanion.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
-    'j-hui/fidget.nvim',
-    {
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'codecompanion' },
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'j-hui/fidget.nvim',
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'codecompanion' },
+        },
+        ft = { 'markdown', 'codecompanion' },
       },
-      ft = { 'markdown', 'codecompanion' },
     },
-  },
-  opts = {
-    strategies = {
-      chat = { adapter = 'ollama', roles = {
-        user = 'fsenat',
-      } },
-      inline = { adapter = 'ollama' },
-    },
-    opts = { stream = true },
-    display = {
-      chat = {
-        window = {
-          position = 'right',
-          width = 0.4,
+    opts = {
+      strategies = {
+        chat = { adapter = 'ollama', roles = {
+          user = 'fsenat',
+        } },
+        inline = { adapter = 'ollama' },
+      },
+      opts = { stream = true },
+      display = {
+        chat = {
+          window = {
+            position = 'right',
+            width = 0.4,
+          },
         },
       },
-      diff = {
-        enabled = true,
-        close_chat_at = 240,
-        layout = 'vertical',
-        opts = { 'internal', 'filler', 'closeoff', 'algorithm:patience', 'followwrap', 'linematch:120' },
-        provider = 'default', -- default|mini_diff
-      },
+    },
+    init = function()
+      require('plugins.codecompanion.spinner'):init()
+    end,
+    keys = {
+      { '<leader>aa', '<cmd>CodeCompanionChat Toggle<cr>', desc = '[T]oggle', mode = { 'n', 'v' } },
+      { 'ga', '<cmd>CodeCompanionActions<cr>', desc = 'Ai [A]ctions', mode = { 'n', 'v' } },
     },
   },
-  init = function()
-    require('plugins.fidget'):init()
-  end,
-  keys = {
-    { '<leader>at', '<cmd>CodeCompanionChat Toggle<cr>', desc = '[T]oggle', mode = { 'n', 'v' } },
-    { '<leader>aa', '<cmd>CodeCompanionChat Add<cr>', desc = '[A]dd', mode = { 'n', 'v' } },
-    { 'ga', '<cmd>CodeCompanionActions<cr>', desc = 'Ai [A]ctions', mode = { 'n', 'v' } },
-  },
+  -- {
+  --   'huggingface/llm.nvim',
+  --   opts = {
+  --     backend = 'ollama',
+  --     model = 'gemma3:4b',
+  --     url = 'http://localhost:11434',
+  --     request_body = {
+  --       -- Modelfile options for the model you use
+  --       options = {
+  --         temperature = 0.2,
+  --         top_p = 0.95,
+  --       },
+  --     },
+  --     lsp = {
+  --       bin_path = vim.api.nvim_call_function('stdpath', { 'data' }) .. '/mason/bin/llm-ls',
+  --     },
+  --   },
+  -- },
 }
