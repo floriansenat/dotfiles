5224a9bdd1ab218e12ed9caae079557a42fd5ad5
Florian SENAT <floriansenat@Florians-MacBook-Pro.local>
2025-03-28 11:30:13 +0100

    make centered layout bigger

--- home/.config/nvim/lua/plugins/zenmode.lua
+++ home/.config/nvim/lua/plugins/zenmode.lua
@@ -1,7 +1,7 @@
 return {
   'folke/zen-mode.nvim',
   lazy = true,
-  opts = { window = { width = 0.6 } },
+  opts = { window = { width = 0.7 } },
   keys = {
     { '<leader>z', '<cmd>ZenMode<CR>', desc = 'Toggle [Z]en mode' },
   },
