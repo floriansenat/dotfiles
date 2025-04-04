0315f88cd185d87686c84d31cb76e9b8f602f7a5
Florian SENAT <floriansenat@Florians-MacBook-Pro.local>
2025-04-04 20:02:34 +0200

    add languages to treesitter

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,44 @@
 local languages = {
-	'lua',
-	'tsx',
-	'typescript',
-	'javascript',
-	'json',
-	'html',
-	'css',
-	'vimdoc',
-	'vim',
-	'markdown',
-	'markdown_inline',
-	'astro',
+  'lua',
+  'php',
+  'go',
+  'tsx',
+  'typescript',
+  'javascript',
+  'json',
+  'html',
+  'css',
+  'vimdoc',
+  'vim',
+  'markdown',
+  'markdown_inline',
+  'astro',
 }
 
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			-- Language installation
-			ensure_installed = languages,
-			auto_install = true, -- Auto-install missing parsers
-
-			-- Core functionality
-			highlight = { enable = true },
-			indent = { enable = true },
-
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-					keymaps = {
-						['aa'] = '@parameter.outer',
-						['ia'] = '@parameter.inner',
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
-					},
-				},
-			},
-		}
-	end,
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = languages,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+      },
+    }
+  end,
 }
