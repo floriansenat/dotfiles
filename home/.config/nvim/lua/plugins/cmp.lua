ab8859e013304330cabd1b07bb8dc0ca54cab720
floriansenat <github.conductor645@passmail.net>
2025-08-11 13:55:47 +0200

    update neovim completion config

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,28 +1,27 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'neovim/nvim-lspconfig',
+    },
     version = '1.*',
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = {
         preset = 'default',
       },
       appearance = {
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
       completion = {
+        list = { selection = { preselect = true, auto_insert = true } },
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
-        accept = { auto_brackets = { enabled = true } },
+        accept = { auto_brackets = { enabled = false } },
       },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
+      signature = { enabled = true },
     },
     opts_extend = { 'sources.default' },
   },
